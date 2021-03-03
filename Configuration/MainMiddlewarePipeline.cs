0934c7ee8d1e5174a6d45b1e05d36ef7bdbe6b3c
Janusz Marcinik <januszmarcinik@gmail.com>
2021-03-03 18:16:52 +0100

    Added Correlation ID middleware

--- Configuration/MainMiddlewarePipeline.cs
+++ Configuration/MainMiddlewarePipeline.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -29,11 +28,7 @@ namespace NETCore3.Configuration
 
             app.UseWhen(context => context.Request.Path.Value.Contains("team-members"), appBuilder =>
             {
-                appBuilder.Use(async (context, next) =>
-                {
-                    context.Request.Headers.Add("correlation-id", $"{Guid.NewGuid()}");
-                    await next.Invoke();
-                });
+                appBuilder.UseCorrelationIdMiddleware();
             });
 
             app.Use(async (context, next) =>
---
+++ Middleware/CorrelationIdMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using NETCore3.Services;
+
+namespace NETCore3.Middleware
+{
+    internal class CorrelationIdMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly CorrelationIdProvider _staticCorrelationIdMiddleware;
+        private readonly ILogger _logger;
+
+        public CorrelationIdMiddleware(
+            RequestDelegate next, 
+            CorrelationIdProvider staticCorrelationIdMiddleware, 
+            ILogger<CorrelationIdMiddleware> logger)
+        {
+            _next = next;
+            _staticCorrelationIdMiddleware = staticCorrelationIdMiddleware;
+            _logger = logger;
+        }
+
+        public Task InvokeAsync(HttpContext context, CorrelationIdProvider scopedCorrelationIdProvider)
+        {
+            _logger.LogInformation("Static correlation ID: {CorrelationId}", _staticCorrelationIdMiddleware.CorrelationId);
+            context.Request.Headers.Add("correlation-id", $"{scopedCorrelationIdProvider.CorrelationId}");
+            
+            return _next.Invoke(context);
+        }
+    }
+    
+    internal static class CorrelationIdMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCorrelationIdMiddleware(this IApplicationBuilder app) =>
+            app.UseMiddleware<CorrelationIdMiddleware>();
+    }
+}
